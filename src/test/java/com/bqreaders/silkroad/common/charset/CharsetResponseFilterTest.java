0c76fb146cfc8b271bb22a6a00eebf79f2d703ad
Alexander De Leon <me@alexdeleon.name>
2014-06-24 14:39:39 +0200

    Tuneup CharsetResponseFilter

--- src/test/java/com/bqreaders/silkroad/common/charset/CharsetResponseFilterTest.java
+++ src/test/java/com/bqreaders/silkroad/common/charset/CharsetResponseFilterTest.java
@@ -3,18 +3,21 @@
  */
 package com.bqreaders.silkroad.common.charset;
 
-import com.sun.jersey.core.header.OutBoundHeaders;
-import com.sun.jersey.spi.container.ContainerRequest;
-import com.sun.jersey.spi.container.ContainerResponse;
-import org.junit.Before;
-import org.junit.Test;
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Collections;
 
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 
-import static org.fest.assertions.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.sun.jersey.core.header.OutBoundHeaders;
+import com.sun.jersey.spi.container.ContainerRequest;
+import com.sun.jersey.spi.container.ContainerResponse;
 
 /**
  * @author Alberto J. Rubio
@@ -35,19 +38,30 @@ public class CharsetResponseFilterTest {
 
 	@Test
 	public void testCharsetResponseFilterWithAtomXml() {
-        CharsetResponseFilter filter = new CharsetResponseFilter();
+		CharsetResponseFilter filter = new CharsetResponseFilter();
 		when(request.getMethod()).thenReturn("GET");
-        when(response.getMediaType()).thenReturn(javax.ws.rs.core.MediaType.APPLICATION_ATOM_XML_TYPE);
+		when(response.getMediaType()).thenReturn(javax.ws.rs.core.MediaType.APPLICATION_ATOM_XML_TYPE);
 		filter.filter(request, response);
-		assertThat(responseHeaders.getFirst("Content-Type")).isEqualTo("application/atom+xml;charset=UTF-8");
+		assertThat(responseHeaders.getFirst("Content-Type")).isEqualTo("application/atom+xml; charset=UTF-8");
 	}
 
-    @Test
-    public void testCharsetResponseFilterWithUrlEncoded() {
-        CharsetResponseFilter filter = new CharsetResponseFilter();
-        when(request.getMethod()).thenReturn("GET");
-        when(response.getMediaType()).thenReturn(MediaType.APPLICATION_FORM_URLENCODED_TYPE);
-        filter.filter(request, response);
-        assertThat(responseHeaders.getFirst("Content-Type")).isEqualTo("application/x-www-form-urlencoded;charset=UTF-8");
-    }
+	@Test
+	public void testCharsetResponseFilterWithUrlEncoded() {
+		CharsetResponseFilter filter = new CharsetResponseFilter();
+		when(request.getMethod()).thenReturn("GET");
+		when(response.getMediaType()).thenReturn(MediaType.APPLICATION_FORM_URLENCODED_TYPE);
+		filter.filter(request, response);
+		assertThat(responseHeaders.getFirst("Content-Type")).isEqualTo(
+				"application/x-www-form-urlencoded; charset=UTF-8");
+	}
+
+	@Test
+	public void testCharsetDefinedInRespinse() {
+		CharsetResponseFilter filter = new CharsetResponseFilter();
+		when(request.getMethod()).thenReturn("GET");
+		when(response.getMediaType()).thenReturn(
+				new MediaType("application", "json", Collections.singletonMap("charset", "UTF-16")));
+		filter.filter(request, response);
+		assertThat(responseHeaders.getFirst("Content-Type")).isEqualTo("application/json; charset=UTF-16");
+	}
 }
