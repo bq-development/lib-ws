fc6803bb879af180348ec7043dfe8ad0b19182d7
Alexander De Leon <me@alexdeleon.name>
2014-06-24 14:39:39 +0200

    Tuneup CharsetResponseFilter

--- src/main/java/com/bqreaders/silkroad/common/charset/CharsetResponseFilter.java
+++ src/main/java/com/bqreaders/silkroad/common/charset/CharsetResponseFilter.java
@@ -1,20 +1,28 @@
 package com.bqreaders.silkroad.common.charset;
 
+import javax.ws.rs.core.MediaType;
+
 import com.sun.jersey.spi.container.ContainerRequest;
 import com.sun.jersey.spi.container.ContainerResponse;
 import com.sun.jersey.spi.container.ContainerResponseFilter;
-
-import javax.ws.rs.core.MediaType;
 
 /**
  * Created by Alberto J. Rubio
  */
 public class CharsetResponseFilter implements ContainerResponseFilter {
 
-    @Override
-    public ContainerResponse filter(ContainerRequest request, ContainerResponse response) {
-        MediaType contentType = response.getMediaType();
-        response.getHttpHeaders().putSingle("Content-Type", contentType.toString() + ";charset=UTF-8");
-        return response;
-    }
+	private static final String CHARSET_UTF_8 = "; charset=UTF-8";
+
+	@Override
+	public ContainerResponse filter(ContainerRequest request, ContainerResponse response) {
+		MediaType mediaType = response.getMediaType();
+		if (mediaType != null) {
+			String contentType = mediaType.toString();
+			if (!contentType.contains("charset")) {
+				contentType = contentType + CHARSET_UTF_8;
+			}
+			response.getHttpHeaders().putSingle("Content-Type", contentType);
+		}
+		return response;
+	}
 }
