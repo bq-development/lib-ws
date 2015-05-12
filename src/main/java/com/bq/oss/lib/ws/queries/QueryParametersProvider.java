7c4e5689f6ebe5b9a20261a34f09d0a97a6889c5
Francisco Sanchez <francisco.sanchez@bq.com>
2015-05-12 16:23:21 +0200

    Lib-ws now support multiple api:query
    Change-Id: I8e3fb33b6123b4c79b7b78636a7f16483cfdb5ed

--- src/main/java/com/bq/oss/lib/ws/queries/QueryParametersProvider.java
+++ src/main/java/com/bq/oss/lib/ws/queries/QueryParametersProvider.java
@@ -1,10 +1,7 @@
 package com.bq.oss.lib.ws.queries;
 
 
-import java.lang.Exception;
-import java.lang.Integer;
-import java.lang.Override;
-import java.lang.String;
+import java.util.List;
 import java.util.Optional;
 
 import javax.ws.rs.WebApplicationException;
@@ -13,13 +10,15 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.Provider;
 
-import com.bq.oss.lib.queries.jaxrs.QueryParameters;
 import com.bq.oss.lib.queries.exception.InvalidParameterException;
+import com.bq.oss.lib.queries.jaxrs.QueryParameters;
 import com.bq.oss.lib.queries.parser.AggregationParser;
 import com.bq.oss.lib.queries.parser.QueryParser;
+import com.bq.oss.lib.queries.parser.SortParser;
 import com.bq.oss.lib.ws.annotation.Rest;
 import com.bq.oss.lib.ws.api.error.ErrorMessage;
 import com.bq.oss.lib.ws.api.error.ErrorResponseFactory;
+import com.bq.oss.lib.ws.model.Error;
 import com.sun.jersey.api.core.HttpContext;
 import com.sun.jersey.api.model.Parameter;
 import com.sun.jersey.core.spi.component.ComponentContext;
@@ -27,71 +26,70 @@ import com.sun.jersey.core.spi.component.ComponentScope;
 import com.sun.jersey.server.impl.inject.AbstractHttpContextInjectable;
 import com.sun.jersey.spi.inject.Injectable;
 import com.sun.jersey.spi.inject.InjectableProvider;
-import com.bq.oss.lib.ws.model.Error;
 
 /**
  * @author Rubén Carrasco
- * 
  */
 @Provider
 public final class QueryParametersProvider implements InjectableProvider<Rest, Parameter> {
 
-	public static final String API_PAGE_SIZE = "api:pageSize";
-	public static final String API_PAGE = "api:page";
-	public static final String API_SORT = "api:sort";
-	public static final String API_QUERY = "api:query";
-	public static final String API_SEARCH = "api:search";
-	public static final String API_AGGREGATION = "api:aggregation";
+    public static final String API_PAGE_SIZE = "api:pageSize";
+    public static final String API_PAGE = "api:page";
+    public static final String API_SORT = "api:sort";
+    public static final String API_QUERY = "api:query";
+    public static final String API_SEARCH = "api:search";
+    public static final String API_AGGREGATION = "api:aggregation";
 
-	private final int defaultPageSize;
-	private final int maxPageSize;
-	private final QueryParser queryParser;
-	private final AggregationParser aggregationParser;
+    private final int defaultPageSize;
+    private final int maxPageSize;
+    private final QueryParser queryParser;
+    private final AggregationParser aggregationParser;
+    private final SortParser sortParser;
 
-	public QueryParametersProvider(int defaultPageSize, int maxPageSize, QueryParser queryParser,
-			AggregationParser aggregationParser) {
-		super();
-		this.defaultPageSize = defaultPageSize;
-		this.maxPageSize = maxPageSize;
-		this.queryParser = queryParser;
-		this.aggregationParser = aggregationParser;
+    public QueryParametersProvider(int defaultPageSize, int maxPageSize, QueryParser queryParser,
+                                   AggregationParser aggregationParser, SortParser sortParser) {
+        super();
+        this.defaultPageSize = defaultPageSize;
+        this.maxPageSize = maxPageSize;
+        this.queryParser = queryParser;
+        this.aggregationParser = aggregationParser;
+        this.sortParser = sortParser;
 
-	}
+    }
 
-	@Override
-	public ComponentScope getScope() {
-		return ComponentScope.PerRequest;
-	}
 
-	@Override
-	public Injectable<QueryParameters> getInjectable(ComponentContext ic, final Rest a, final Parameter c) {
+    @Override
+    public ComponentScope getScope() {
+        return ComponentScope.PerRequest;
+    }
 
-		if (QueryParameters.class != c.getParameterClass()) {
-			return null;
-		}
+    @Override
+    public Injectable<QueryParameters> getInjectable(ComponentContext ic, final Rest a, final Parameter c) {
 
-		return new AbstractHttpContextInjectable<QueryParameters>() {
+        if (QueryParameters.class != c.getParameterClass()) {
+            return null;
+        }
 
-			@Override
-			public QueryParameters getValue(HttpContext context) {
-				MultivaluedMap<String, String> params = context.getUriInfo().getQueryParameters();
+        return new AbstractHttpContextInjectable<QueryParameters>() {
 
-				try {
-					return new QueryParameters(getIntegerParam(params, API_PAGE_SIZE).orElse(defaultPageSize),
-							getIntegerParam(params, API_PAGE).orElse(0), maxPageSize, getStringParam(params, API_SORT),
-							getStringParam(params, API_QUERY), queryParser, getStringParam(params, API_AGGREGATION),
-							aggregationParser, getStringParam(params, API_SEARCH));
-                }
-                catch (InvalidParameterException e) {
+            @Override
+            public QueryParameters getValue(HttpContext context) {
+                MultivaluedMap<String, String> params = context.getUriInfo().getQueryParameters();
+
+                try {
+                    return new QueryParameters(getIntegerParam(params, API_PAGE_SIZE).orElse(defaultPageSize),
+                            getIntegerParam(params, API_PAGE).orElse(0), maxPageSize, getStringParam(params, API_SORT),
+                            getListStringParam(params, API_QUERY), queryParser, getStringParam(params, API_AGGREGATION),
+                            aggregationParser, sortParser, getStringParam(params, API_SEARCH));
+                } catch (InvalidParameterException e) {
                     throw toRequestException(e);
-                }
-                catch (IllegalArgumentException e) {
+                } catch (IllegalArgumentException e) {
                     throw new WebApplicationException(badRequestResponse(error(e)));
                 }
-			}
+            }
 
-            private WebApplicationException toRequestException(InvalidParameterException e){
-                switch(e.getParameter()){
+            private WebApplicationException toRequestException(InvalidParameterException e) {
+                switch (e.getParameter()) {
                     case AGGREGATION:
                         return new WebApplicationException(badRequestResponse(new Error("invalid_aggregation",
                                 ErrorMessage.INVALID_AGGREGATION.getMessage(e.getValue(), e.getMessage()))));
@@ -111,22 +109,27 @@ public final class QueryParametersProvider implements InjectableProvider<Rest, P
                 }
             }
 
-			private Error error(Exception e) {
-				return new Error("bad_request", e.getMessage());
-			}
+            private Error error(Exception e) {
+                return new Error("bad_request", e.getMessage());
+            }
 
-			private Response badRequestResponse(Error error) {
-				return Response.status(Status.BAD_REQUEST).entity(error).build();
-			}
+            private Response badRequestResponse(Error error) {
+                return Response.status(Status.BAD_REQUEST).entity(error).build();
+            }
 
-			private Optional<java.lang.Integer> getIntegerParam(MultivaluedMap<String, String> params, String key) {
-				return params.containsKey(key) ? Optional.of(Integer.valueOf(params.get(key).get(0))) : Optional
-						.empty();
-			}
+            private Optional<java.lang.Integer> getIntegerParam(MultivaluedMap<String, String> params, String key) {
+                return params.containsKey(key) ? Optional.of(Integer.valueOf(params.get(key).get(0))) : Optional
+                        .empty();
+            }
 
-			private Optional<String> getStringParam(MultivaluedMap<String, String> params, String key) {
-				return params.containsKey(key) ? Optional.of(params.get(key).get(0)) : Optional.empty();
-			}
-		};
-	}
+            private Optional<String> getStringParam(MultivaluedMap<String, String> params, String key) {
+                return params.containsKey(key) ? Optional.of(params.get(key).get(0)) : Optional.empty();
+            }
+
+            private Optional<List<String>> getListStringParam(MultivaluedMap<String, String> params, String key) {
+                return params.containsKey(key) ? Optional.of(params.get(key)) : Optional.empty();
+
+            }
+        };
+    }
 }
