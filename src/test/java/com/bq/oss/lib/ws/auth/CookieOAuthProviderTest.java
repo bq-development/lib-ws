79f91287027dd2dde3a91fd0eb993674a21b32db
Ismael Madirolas <ismael.madirolas@bq.com>
2015-06-26 15:21:28 +0200

    dropwizard 0.8.1
    Change-Id: I8a2db002d9831845ce27320981283f960f5ea3dd

--- src/test/java/com/bq/oss/lib/ws/auth/CookieOAuthProviderTest.java
+++ src/test/java/com/bq/oss/lib/ws/auth/CookieOAuthProviderTest.java
@@ -3,25 +3,19 @@ package com.bq.oss.lib.ws.auth;
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import io.dropwizard.auth.Auth;
+import io.dropwizard.auth.Authenticator;
 
-import java.util.HashMap;
-import java.util.Map;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Cookie;
-
+import org.glassfish.jersey.server.model.Parameter;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.google.common.base.Optional;
-import com.sun.jersey.api.core.HttpContext;
-import com.sun.jersey.api.core.HttpRequestContext;
-import com.sun.jersey.api.model.Parameter;
-import com.sun.jersey.core.spi.component.ComponentContext;
-import com.sun.jersey.server.impl.inject.AbstractHttpContextInjectable;
-import com.sun.jersey.spi.inject.Injectable;
-import io.dropwizard.auth.Auth;
-import io.dropwizard.auth.Authenticator;
+import com.sun.jndi.toolkit.ctx.ComponentContext;
+
 
 /**
  * @author Rubén Carrasco
@@ -29,66 +23,51 @@ import io.dropwizard.auth.Authenticator;
  */
 public class CookieOAuthProviderTest {
 
-	private static final String TOKEN = "token";
+    private static final String TOKEN = "tokenTest";
 
-	CookieOAuthProvider<TestClass> cookieOAuthProvider;
+    CookieOAuthFactory<TestClass> cookieOAuthProvider;
 
-	private Authenticator<String, TestClass> authenticator;
+    private Authenticator<String, TestClass> authenticator;
 
-	private Cookie cookie;
+    private HttpServletRequest request;
 
-	private HttpContext context;
+    private Auth a;
 
-	private HttpRequestContext request;
+    @Before
+    public void setUp() throws Exception {
+        authenticator = mock(Authenticator.class);
+        Optional<TestClass> value = Optional.of(new TestClass());
+        when(authenticator.authenticate(TOKEN)).thenReturn(value);
+        cookieOAuthProvider = new CookieOAuthFactory<TestClass>(authenticator, "realm", TestClass.class);
 
-	private Auth a;
+        a = mock(Auth.class);
+        when(a.required()).thenReturn(true);
 
-	@Before
-	public void setUp() throws Exception {
-		authenticator = mock(Authenticator.class);
-		Optional<TestClass> value = Optional.of(new TestClass());
-		when(authenticator.authenticate(TOKEN)).thenReturn(value);
-		cookieOAuthProvider = new CookieOAuthProvider<>(authenticator);
+        request = mock(HttpServletRequest.class);
+        cookieOAuthProvider.setRequest(request);
+        Cookie[] cookies = new Cookie[] {new Cookie("token", TOKEN)};
+        when(request.getCookies()).thenReturn(cookies);
+    }
 
-		a = mock(Auth.class);
-		when(a.required()).thenReturn(true);
+    @Test
+    public void test() {
+        ComponentContext ic = null;
+        Parameter c = null;
+        assertThat(cookieOAuthProvider.provide()).isInstanceOf(TestClass.class);
+    }
 
-		cookie = mock(Cookie.class);
-		when(cookie.getValue()).thenReturn(TOKEN);
+    @Test
+    public void testNoCookie() {
+        ComponentContext ic = null;
+        Parameter c = null;
 
-		context = mock(HttpContext.class);
-		request = mock(HttpRequestContext.class);
-		when(context.getRequest()).thenReturn(request);
-		Map<String, Cookie> cookies = new HashMap<String, Cookie>() {
-			{
-				put(TOKEN, cookie);
-			}
-		};
-		when(request.getCookies()).thenReturn(cookies);
-	}
+        when(request.getCookies()).thenReturn(null);
 
-	@Test
-	public void test() {
-		ComponentContext ic = null;
-		Parameter c = null;
-		Injectable<?> injectable = cookieOAuthProvider.getInjectable(ic, a, c);
-		Object value = ((AbstractHttpContextInjectable) injectable).getValue(context);
-		assertThat(value instanceof TestClass).isTrue();
-	}
+        assertThat(cookieOAuthProvider.provide()).isNull();
+    }
 
-	@Test(expected = WebApplicationException.class)
-	public void testNoCookie() {
-		ComponentContext ic = null;
-		Parameter c = null;
-		Injectable<?> injectable = cookieOAuthProvider.getInjectable(ic, a, c);
+    private class TestClass {
 
-		when(request.getCookies()).thenReturn(null);
-
-		Object value = ((AbstractHttpContextInjectable) injectable).getValue(context);
-	}
-
-	private class TestClass {
-
-	}
+    }
 
 }
