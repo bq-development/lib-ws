79b19b58c0624631c9fee8b554b2cef1f6a5c28e
Francisco Sanchez <francisco.sanchez@bq.com>
2015-07-13 18:09:00 +0200

    add priority to empty message jackson meesage body provider
    Change-Id: Ic94c4a049224219867b36b9976b6d997a3928b95

--- src/main/java/com/bq/oss/lib/ws/cli/ServiceRunner.java
+++ src/main/java/com/bq/oss/lib/ws/cli/ServiceRunner.java
@@ -49,9 +49,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  * 
  */
 public abstract class ServiceRunner<T> {
-
-
     private static final Logger LOG = LoggerFactory.getLogger(ServiceRunner.class);
+    private static final int HIGH_PRIORITY = 1;
     private final ServiceRunnerApplication application = new ServiceRunnerApplication();
 
     public final void run(String[] arguments) throws Exception {
@@ -85,8 +84,8 @@ public abstract class ServiceRunner<T> {
 
     private void configureDefaultProviders(Environment environment) {
         environment.jersey().register(new GsonMessageReaderWriterProvider());
-        environment.jersey().register(
-                new EmptyEntitiesAllowedJacksonMessageBodyProvider(environment.getObjectMapper(), environment.getValidator()));
+        // EmptyEntitiesAllowedJacksonMessageBodyProvider Override JacksonMessageBodyProvider to allow null entities
+        environment.jersey().getResourceConfig().register(new EmptyEntitiesAllowedJacksonMessageBodyProvider(environment.getObjectMapper(), environment.getValidator()), HIGH_PRIORITY);
     }
 
     private void configureDropWizard(Configuration configuration, ApplicationContext applicationContext) {
