ae71f27f3a48966f2bda7f885c97de97f555f129
Alberto J. Rubio <yebenes9@gmail.com>
2015-09-01 17:15:44 +0200

    Update version & delete comment with javadoc problem.
    Change-Id: I502ccccdb0424cfcd2762427649c25b5cbc1420c

--- src/main/java/io/corbel/lib/ws/filter/ChunkedAwaredShallowEtagHeaderFilter.java
+++ src/main/java/io/corbel/lib/ws/filter/ChunkedAwaredShallowEtagHeaderFilter.java
@@ -9,9 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * @author Alexander De Leon <alex.deleon@devialab.com>
- */
 public class ChunkedAwaredShallowEtagHeaderFilter extends ShallowEtagHeaderFilter {
 
     @Override
