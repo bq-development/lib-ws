070427b513f64b0b7bf4acfc8367e83f93ef87ee
Ismael Madirolas <ismael.madirolas@bq.com>
2015-10-27 11:01:32 +0100

    better healthcheck for redis
    Change-Id: I4cc4772db72d1291a3b7a9ee08ef9d468101038f

--- src/main/java/io/corbel/lib/ws/health/AuthorizationRedisHealthCheck.java
+++ src/main/java/io/corbel/lib/ws/health/AuthorizationRedisHealthCheck.java
@@ -1,8 +1,8 @@
 package io.corbel.lib.ws.health;
 
-import com.codahale.metrics.health.HealthCheck;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import com.codahale.metrics.health.HealthCheck;
 import com.google.gson.JsonObject;
 
 /**
@@ -11,17 +11,18 @@ import com.google.gson.JsonObject;
  */
 public class AuthorizationRedisHealthCheck extends HealthCheck {
 
-	private final RedisTemplate<String, JsonObject> redisTemplate;
+    private final RedisTemplate<String, JsonObject> redisTemplate;
 
-	public AuthorizationRedisHealthCheck(RedisTemplate<String, JsonObject> redisTemplate) {
-		this.redisTemplate = redisTemplate;
-	}
+    public AuthorizationRedisHealthCheck(RedisTemplate<String, JsonObject> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
 
-	@Override
-	protected Result check() throws Exception {
-		if (redisTemplate.getConnectionFactory().getConnection().ping().equals("PONG")) {
-			return Result.healthy();
-		}
-		return Result.unhealthy("Redis is down");
-	}
+    @Override
+    protected Result check() throws Exception {
+        if (redisTemplate.getConnectionFactory().getConnection().ping().equals("PONG")
+                && redisTemplate.opsForSet().members("fake").size() == 0) {
+            return Result.healthy();
+        }
+        return Result.unhealthy("Redis is down");
+    }
 }
