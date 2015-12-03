6f495b72f17954ff76d26840a7a09b546ec69d57
Ismael Madirolas <ismael.madirolas@bq.com>
2015-12-03 12:22:27 +0100

    fix domain replacement in scopes filling
    Change-Id: Ifa718a3c155a672dab6cf960f5efbb436e58b9aa

--- src/main/java/io/corbel/lib/ws/auth/AuthorizationRequestFilter.java
+++ src/main/java/io/corbel/lib/ws/auth/AuthorizationRequestFilter.java
@@ -1,17 +1,35 @@
 package io.corbel.lib.ws.auth;
 
 import static java.util.stream.StreamSupport.stream;
+import io.corbel.lib.ws.api.error.ErrorResponseFactory;
+import io.dropwizard.auth.oauth.OAuthFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.Principal;
-import java.util.*;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 import javax.annotation.Priority;
-import javax.servlet.*;
-import javax.servlet.http.*;
+import javax.servlet.AsyncContext;
+import javax.servlet.DispatcherType;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpUpgradeHandler;
+import javax.servlet.http.Part;
 import javax.ws.rs.HttpMethod;
 import javax.ws.rs.Priorities;
 import javax.ws.rs.WebApplicationException;
@@ -24,15 +42,12 @@ import javax.ws.rs.core.Response;
 import org.eclipse.jetty.http.HttpHeader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.CollectionUtils;
 
 import com.google.common.collect.Sets;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-
-import io.corbel.lib.ws.api.error.ErrorResponseFactory;
-import io.dropwizard.auth.oauth.OAuthFactory;
-import org.springframework.util.CollectionUtils;
 
 /**
  * This class is a bit of a hack to Dropwizard(Jersey 2.17). It uses the {@link io.dropwizard.auth.oauth.OAuthFactory} class to obtain an
@@ -45,10 +60,13 @@ import org.springframework.util.CollectionUtils;
  */
 @Priority(Priorities.AUTHORIZATION) public class AuthorizationRequestFilter implements ContainerRequestFilter {
 
+    private static final Logger LOG = LoggerFactory.getLogger(AuthorizationRequestFilter.class);
+
     public static final String AUTHORIZATION_INFO_PROPERTIES_KEY = "AuthorizationInfo";
 
     private static final Pattern REQUEST_WITH_DOMAIN_PATTERN = Pattern.compile("v[0-9]+\\.[0-9]+/[\\w\\-:\\.]+/\\w+/\\w+:.+");
-    private static final Logger LOG = LoggerFactory.getLogger(AuthorizationRequestFilter.class);
+    private static final String VERSION_REGEX = "v[0-9]+\\.[0-9]+/";
+    private static final String EMPTY_STRING = "";
 
     private final OAuthFactory<AuthorizationInfo> oAuthProvider;
     private final CookieOAuthFactory<AuthorizationInfo> cookieOAuthProvider;
@@ -68,7 +86,7 @@ import org.springframework.util.CollectionUtils;
     }
 
     public AuthorizationRequestFilter(OAuthFactory<AuthorizationInfo> provider, CookieOAuthFactory<AuthorizationInfo> cookieOAuthProvider,
-                                      String unAuthenticatedPathPattern, boolean checkDomain) {
+            String unAuthenticatedPathPattern, boolean checkDomain) {
         this(provider, cookieOAuthProvider, null, unAuthenticatedPathPattern, checkDomain);
     }
 
@@ -125,7 +143,7 @@ import org.springframework.util.CollectionUtils;
 
     public void checkPublicAccessRules(String domainId, final ContainerRequestContext request) {
         Set<JsonObject> applicableRules = null;
-        if(publicAccessService != null) {
+        if (publicAccessService != null) {
             Set<JsonObject> accessRules = publicAccessService.getDomainPublicRules(domainId);
             applicableRules = getApplicableAccessRules(accessRules, request, domainId, false);
         }
@@ -135,10 +153,11 @@ import org.springframework.util.CollectionUtils;
         }
     }
 
-    public Set<JsonObject> getApplicableAccessRules(Set<JsonObject> accessRules, final ContainerRequestContext request, String domainId, boolean userToken) {
+    public Set<JsonObject> getApplicableAccessRules(Set<JsonObject> accessRules, final ContainerRequestContext request, String domainId,
+            boolean userToken) {
         String scopeUrl = extractScopeUrl(domainId, request.getUriInfo().getPath());
-        return Sets.filter(accessRules, rule -> matchesMethod(request.getMethod(), rule) &&
-                matchesUriPath(scopeUrl, rule) && matchesMediaTypes(request, rule) && (matchesTokenType(userToken, rule)));
+        return Sets.filter(accessRules, rule -> matchesMethod(request.getMethod(), rule) && matchesUriPath(scopeUrl, rule)
+                && matchesMediaTypes(request, rule) && (matchesTokenType(userToken, rule)));
     }
 
     private Response generateInvalidTokenResponse() {
@@ -150,9 +169,8 @@ import org.springframework.util.CollectionUtils;
     }
 
     private String extractScopeUrl(String domainId, String path) {
-        if (domainId != null)
-            path = path.replace(domainId + "/", "");
-        return path.substring(path.indexOf("/") + 1);
+        String regex = domainId != null ? VERSION_REGEX + "(" + domainId + "/)?" : VERSION_REGEX;
+        return path.replaceFirst(regex, EMPTY_STRING);
     }
 
     private void storeAuthorizationInfoInRequestProperties(AuthorizationInfo info, ContainerRequestContext request) {
@@ -195,8 +213,8 @@ import org.springframework.util.CollectionUtils;
         JsonArray mediaTypesArray = input.get("mediaTypes").getAsJsonArray();
 
         for (MediaType mediaType : request.getAcceptableMediaTypes()) {
-            if (stream(mediaTypesArray.spliterator(), true)
-                    .map(mediaTypeElement -> MediaType.valueOf(mediaTypeElement.getAsString())).anyMatch(mediaType::isCompatible)) {
+            if (stream(mediaTypesArray.spliterator(), true).map(mediaTypeElement -> MediaType.valueOf(mediaTypeElement.getAsString()))
+                    .anyMatch(mediaType::isCompatible)) {
                 return true;
             }
         }
